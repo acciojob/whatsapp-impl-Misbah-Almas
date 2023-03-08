7d527074b404ef5667d0bd8b7950d37e5e1fd39c
Misbah Almas <dev.misbahalmas@gmail.com>
2023-03-08 16:51:03 +0530

    Solved bonus problem

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -93,4 +93,74 @@ public class WhatsappRepository {
 
         return "SUCCESS";
     }
+
+    public int removeUser(User user) throws Exception {
+        if(isNewUser(user.getMobile())){
+            throw new Exception("User not found");
+        }
+        Group group = new Group();
+        for(Group itr : groupUserMap.keySet()){
+            if(groupUserMap.get(itr).contains(user)){
+                group = itr;
+            }
+        }
+        if(user.equals(groupUserMap.get(group).get(0))){
+            throw new Exception("Cannot remove admin");
+        }
+
+        List<Message> messageList = new ArrayList<>();
+        int userCount = 0;
+        int messageCount = 0;
+        int totalMessage = 0;
+        if(!user.equals(groupUserMap.get(group).get(0))){
+            groupUserMap.get(group).remove(user);
+            userDb.remove(user.getMobile());
+            for(Message message : senderMap.keySet()){
+                if(senderMap.get(message).equals(user)){
+                    messageList.add(message);
+                }
+            }
+            for(Message message : messageList){
+                groupMessageMap.get(group).remove(message);
+                senderMap.remove(message);
+                messageDb.remove(message.getId());
+            }
+
+            if(groupUserMap.get(group).size()<=2){
+                this.customGroupCount--;
+                if(groupUserMap.get(group).size()<2){
+                    groupUserMap.remove(group);
+                    groupUserMap.remove(group);
+                    adminMap.remove(group);
+                }
+            }
+
+            if(groupUserMap.containsKey(group)){
+                userCount = groupUserMap.get(group).size();
+                messageCount = groupMessageMap.get(group).size();
+                totalMessage = messageId - messageList.size();
+            }
+        }
+        return (userCount + messageCount + totalMessage);
+    }
+
+    public String findMessage(Date start, Date end, int k) throws Exception {
+        List<Message> messageList = new ArrayList<>();
+        for(Message message : messageDb.values()){
+            if(message.getTimestamp().after(start) && message.getTimestamp().before(end)){
+                messageList.add(message);
+            }
+        }
+        if(messageList.size() < k){
+            throw new Exception("K is greater than the number of messages");
+        }
+        Collections.sort(messageList, new Comparator<Message>() {
+            @Override
+            public int compare(Message o1, Message o2) {
+                return o2.getTimestamp().compareTo(o1.getTimestamp());
+            }
+        });
+
+        return messageList.get(k-1).getContent();
+    }
 }
